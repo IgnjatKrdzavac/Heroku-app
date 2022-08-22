ef0476aae3d7ce253456ed3834a81ac6f4495972
Ignjat Krdzavac <ikrdzavac11520rn@raf.rs>
2022-08-22 03:03:44 +0200

    commit

--- Backend/app.js
+++ Backend/app.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http:///heroku-app77.herokuapp.com",
+        origin: "https:///heroku-app77.herokuapp.com",
         methods: ['GET', 'POST'],
         credentials: true
     },
